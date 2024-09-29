b8f1fcc835d50fce48c3aa7d6344f6007180094b
Sou15 <ashleeagres56@gmail.com>
2024-09-29 09:10:38 -0500

    initial project

---
+++ sw-async-hilos/src/main/java/pe/edu/cibertec/sw_async_hilos/SwAsyncHilosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.sw_async_hilos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwAsyncHilosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwAsyncHilosApplication.class, args);
+	}
+
+}
---
+++ sw-async-hilos/src/test/java/pe/edu/cibertec/sw_async_hilos/SwAsyncHilosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.sw_async_hilos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwAsyncHilosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
